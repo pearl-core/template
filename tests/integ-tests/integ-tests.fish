9d54114ce98c6d74c0da762dd8eeb9610168e73c
Filippo Squillace <squillace.filippo@gmail.com>
2016-11-19 01:59:52 +0000

    :bug: Fix new integ tests

--- tests/integ-tests/integ-tests.fish
+++ tests/integ-tests/integ-tests.fish
@@ -4,9 +4,13 @@ source $HOME/.config/fish/config.fish
 
 pearl install test
 
-# From here is where you can add the integ tests for your packages
-
 if [ ! -d $PEARL_HOME/packages/default/test ]
     echo "Error: The package test does not exist after installing it."
     exit 1
 end
+
+# From here is where you can add the integ tests for your packages
+
+pearl update test
+
+pearl remove test
--- tests/integ-tests/integ-tests.sh
+++ tests/integ-tests/integ-tests.sh
@@ -6,7 +6,10 @@ source $HOME/.bashrc
 
 pearl install test
 
-# From here is where you can add the integ tests for your packages
-
 [ -d $PEARL_HOME/packages/default/test ] || { echo "Error: The package test does not exist after installing it."; exit 1; }
 
+# From here is where you can add the integ tests for your packages
+
+pearl update test
+
+pearl remove test
